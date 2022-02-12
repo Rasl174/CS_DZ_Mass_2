deb9b68ad7e604df501d49e2623fe0ee035e9391
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-12 16:29:34 +0500

    Изменил

--- CS_DZ_Mass_2/Program.cs
+++ CS_DZ_Mass_2/Program.cs
@@ -10,33 +10,45 @@ namespace CS_DZ_Mass_2
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
+            Random random = new Random();
             int[,] matrix = new int[10, 10];
             
             for(int i = 0; i < matrix.GetLength(0); i++)
             {
                 for(int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    matrix[i, j] = rand.Next(0, 10);
+                    matrix[i, j] = random.Next(0, 10);
                     Console.Write(matrix[i,j] + " ");
                 }
                 Console.WriteLine();
             }
-            int maxValue = matrix.Cast<int>().Max();
-            
-            Console.WriteLine("Максимальное значение: " + maxValue);
-            Console.WriteLine("Новый массив: ");
+
+            int maxValue = int.MinValue;
 
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    if (matrix[i, j] == maxValue) matrix[i, j] = 0;
+                    if (matrix[i,j] > maxValue)
+                    {
+                        maxValue = matrix[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("\nМаксимальное значение: " + maxValue);
+            Console.WriteLine("\nНовый массив: \n");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == maxValue) 
+                        matrix[i, j] = 0;
                     Console.Write("{0}", matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-
         }
     }
 }
